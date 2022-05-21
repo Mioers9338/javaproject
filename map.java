c588d88e1fefb3f59e80d34c2701ed380ff59a3e
Mioers9338 <105540458+Mioers9338@users.noreply.github.com>
2022-05-21 14:00:15 +0800

    Update map.java

--- map.java
+++ map.java
@@ -6,9 +6,9 @@ public class map {
     private double[] Wall;
     private List<cell> cells = new ArrayList<>();
 
-    public map(double[] Wall,List<cell> cells) {
+    public map(double[] Wall, List<cell> cells) {
         this.Wall = Wall;
-        this.cells =cells;
+        this.cells = cells;
     }
 
     public double[] getWall() {
@@ -24,41 +24,84 @@ public class map {
     }
 
     public void move(cell c) {
-        double dis = (double)1/15;
+        double dis = (double) 1 / 15;
         for (int i = 0; i < cells.size(); ++i) {
             if (cells.get(i).getidentity() != c.getidentity()) {
                 double dis_i = c.move(cells.get(cells.get(i).getidentity()));
+                if (dis_i < 0) dis_i = 0;
                 if (dis_i < dis) dis = dis_i;
             }
         }
-        if (c.getColor() .equals( Color.RED)) {
+        if (c.getColor().equals(Color.RED)) {
             double[] p = c.getPosition();
-            p[1] = p[1] + (double)1/15;
+            if (p[1] + c.getRadius() > Wall[1] - dis) {
+                p[1] = Wall[1] - c.getRadius();
+            } else {
+                p[1] = p[1] + dis;
+            }
             c.setPosition(p);
         }
-        if (c.getColor() .equals( Color.GREEN)) {
+        if (c.getColor().equals(Color.GREEN)) {
             double[] p = c.getPosition();
-            p[1] = p[1] - (double)1/15;
+            if (p[1] - c.getRadius() < dis) {
+                p[1] = c.getRadius();
+            } else {
+                p[1] = p[1] - dis;
+            }
             c.setPosition(p);
         }
-        if (c.getColor() .equals( Color.BLUE)) {
+        if (c.getColor().equals(Color.BLUE)) {
             double[] p = c.getPosition();
-            p[0] = p[0] - (double)1/15;
+            if (p[0] - c.getRadius() < dis) {
+                p[0] = c.getRadius();
+            } else {
+                p[0] = p[0] - dis;
+            }
             c.setPosition(p);
         }
-        if (c.getColor() .equals(Color.YELLOW)) {
+        if (c.getColor().equals(Color.YELLOW)) {
             double[] p = c.getPosition();
-            p[0] = p[0] + (double)1/15;
+            if (p[0] + c.getRadius() > Wall[1] - dis) {
+                p[1] = Wall[1] - c.getRadius();
+            } else {
+                p[0] = p[0] + dis;
+            }
             c.setPosition(p);
         }
     }
 
-    public void move(){
-        for(int i = 0; i < cells.size(); ++i) {
-            if(cells.get(i).insidewall()) {
+    public void move() {
+        for (int i = 0; i < cells.size(); ++i) {
+            if (cells.get(i).insidewall()) {
                 move(cells.get(i));
             }
         }
     }
+
+    public void ChangeColor() {
+        for (int i = 0; i < cells.size(); ++i) {
+            int[] ColorNumber = new int[4]; //存储检测范围各种颜色的数量
+            for (int j = 0; i < cells.size(); ++i) { //遍历所有细胞
+                if (cells.get(i).getidentity() != cells.get(j).getidentity()) { //判断不是同一个cell
+                    if (cells.get(i).iswithin(cells.get(j))) { //判断在范围内
+                        ColorNumber[colorNumber(cells.get(j).getLastColor())]++; //统计颜色数量
+                    }
+                    cells.get(i).ChangeColor(ColorNumber); //改变颜色
+                }
+            }
+        }
+        for (int i = 0; i < cells.size(); ++i) {
+            cells.get(i).setLastColor(cells.get(i).getColor());
+        }
+    }
+
+    private int colorNumber(Color color) {
+        if (color.equals(Color.RED)) return 0;
+        if (color.equals(Color.GREEN)) return 1;
+        if (color.equals(Color.BLUE)) return 2;
+        if (color.equals(Color.YELLOW)) return 3;
+        return -1;
+    }
+
 
 }
