0e16682e6feb47d96aeaac313393817f0a044614
Mioers9338 <105540458+Mioers9338@users.noreply.github.com>
2022-05-21 13:59:43 +0800

    Update cell.java

--- cell.java
+++ cell.java
@@ -7,12 +7,14 @@ public class cell {
     private Color color;
     private double Perception;
     private double[] Wall;
+    private Color lastColor;
 
-    public cell(int id, double r, double[] p, double perception,Color c , double[] Wall) {
+    public cell(int id, double r, double[] p, double perception, Color c, double[] Wall) {
         this.Identity = id;
         this.Radius = r;
         this.Position = p;
         this.color = c;
+        this.lastColor = c;
         this.Perception = perception;
         this.Wall = Wall;
     }
@@ -50,19 +52,19 @@ public class cell {
 
     public double move_Red(cell b) {
         boolean hit = false;
-        if (b.getPosition()[1] <= Position[1] + (double)1/15 && b.getPosition()[1] >= Position[1]) {
+        if (b.getPosition()[1] <= Position[1] + (double) 1 / 15 && b.getPosition()[1] >= Position[1]) {
             if (Math.abs(b.getPosition()[0] - Position[0]) < (b.getRadius() + Radius)) {
                 hit = true;
             }
         } else {
             double size = Radius + b.getRadius();
             double dis_x = Position[0] - b.getPosition()[0];
-            double dis_y = Position[1] + (double)1/15 - b.getPosition()[1];
+            double dis_y = Position[1] + (double) 1 / 15 - b.getPosition()[1];
             double dis = Math.sqrt(dis_x * dis_x + dis_y * dis_y);
-            if (size <= dis) hit = true;
+            if (size > dis) hit = true;
         }
         if (!hit) {
-            return (double)1/15;
+            return (double) 1 / 15;
         } else {
             double size = Radius + b.getRadius();
             double dis_x = Position[0] - b.getPosition()[0];
@@ -74,67 +76,67 @@ public class cell {
 
     public double move_Green(cell b) {
         boolean hit = false;
-        if (b.getPosition()[1] >= Position[1] - (double)1/15 && b.getPosition()[1] <= Position[1]) {
+        if (b.getPosition()[1] >= Position[1] - (double) 1 / 15 && b.getPosition()[1] <= Position[1]) {
             if (Math.abs(b.getPosition()[0] - Position[0]) < (b.getRadius() + Radius)) {
                 hit = true;
             }
         } else {
             double size = Radius + b.getRadius();
             double dis_x = Position[0] - b.getPosition()[0];
-            double dis_y = Position[1] - (double)1/15 - b.getPosition()[1];
+            double dis_y = Position[1] - (double) 1 / 15 - b.getPosition()[1];
             double dis = Math.sqrt(dis_x * dis_x + dis_y * dis_y);
-            if (size <= dis) hit = true;
+            if (size > dis) hit = true;
         }
         if (!hit) {
-            return (double)1/15;
+            return (double) 1 / 15;
         } else {
             double size = Radius + b.getRadius();
             double dis_x = Position[0] - b.getPosition()[0];
             double dis_y = Position[1] - b.getPosition()[1];
-            return dis_y + Math.sqrt(size * size - dis_x * dis_x);
+            return -dis_y + Math.sqrt(size * size - dis_x * dis_x);
         }
 
     }
 
     public double move_Blue(cell b) {
         boolean hit = false;
-        if (b.getPosition()[0] >= Position[0] - (double)1/15 && b.getPosition()[0] <= Position[0]) {
+        if (b.getPosition()[0] >= Position[0] - (double) 1 / 15 && b.getPosition()[0] <= Position[0]) {
             if (Math.abs(b.getPosition()[1] - Position[1]) < (b.getRadius() + Radius)) {
                 hit = true;
             }
         } else {
             double size = Radius + b.getRadius();
-            double dis_x = Position[0] - (double)1/15 - b.getPosition()[0];
+            double dis_x = Position[0] - (double) 1 / 15 - b.getPosition()[0];
             double dis_y = Position[1] - b.getPosition()[1];
             double dis = Math.sqrt(dis_x * dis_x + dis_y * dis_y);
-            if (size <= dis) hit = true;
+            if (size > dis) hit = true;
         }
         if (!hit) {
-            return (double)1/15;
+            return (double) 1 / 15;
         } else {
             double size = Radius + b.getRadius();
             double dis_x = Position[0] - b.getPosition()[0];
             double dis_y = Position[1] - b.getPosition()[1];
-            return dis_x + Math.sqrt(size * size - dis_y * dis_y);
+            return -dis_x + Math.sqrt(size * size - dis_y * dis_y);
         }
 
     }
 
     public double move_Yellow(cell b) {
         boolean hit = false;
-        if (b.getPosition()[0] <= Position[0] + (double)1/15 && b.getPosition()[0] >= Position[0]) {
+        if (b.getPosition()[0] <= Position[0] + (double) 1 / 15 && b.getPosition()[0] >= Position[0]) {
             if (Math.abs(b.getPosition()[1] - Position[1]) < (b.getRadius() + Radius)) {
                 hit = true;
             }
         } else {
             double size = Radius + b.getRadius();
-            double dis_x = Position[0] + (double)1/15 - b.getPosition()[0];
+            double dis_x = Position[0] + (double) 1 / 15 - b.getPosition()[0];
             double dis_y = Position[1] - b.getPosition()[1];
             double dis = Math.sqrt(dis_x * dis_x + dis_y * dis_y);
-            if (size <= dis) hit = true;
+            if (size > dis) hit = true;
         }
         if (!hit) {
-            return (double)1/15;
+            return (double) 1 / 15;
         } else {
             double size = Radius + b.getRadius();
             double dis_x = Position[0] - b.getPosition()[0];
@@ -144,22 +146,74 @@ public class cell {
 
     }
 
+    public void ChangeColor(int[] colorNumber) {
+        int total = 0;
+        for (int i = 0; i < 4; ++i) {
+            total += colorNumber[i];
+        }
+        if (color.equals(Color.RED)) {
+            if (colorNumber[0] >= 3 && colorNumber[0] * 10 >= total * 7) {
+                color = Color.GREEN;
+                return;
+            }
+            if (colorNumber[3] >= 1 && colorNumber[3] * 10 <= total) {
+                color = Color.YELLOW;
+                return;
+            }
+            return;
+        }
+        if (color.equals(Color.GREEN)) {
+            if (colorNumber[1] >= 3 && colorNumber[1] * 10 >= total * 7) {
+                color = Color.BLUE;
+                return;
+            }
+            if (colorNumber[0] >= 1 && colorNumber[0] * 10 <= total) {
+                color = Color.RED;
+                return;
+            }
+            return;
+        }
+        if (color.equals(Color.BLUE)) {
+            if (colorNumber[2] >= 3 && colorNumber[2] * 10 >= total * 7) {
+                color = Color.YELLOW;
+                return;
+            }
+            if (colorNumber[1] >= 1 && colorNumber[1] * 10 <= total) {
+                color = Color.GREEN;
+                return;
+            }
+            return;
+        }
+        if (color.equals(Color.YELLOW)) {
+            if (colorNumber[3] >= 3 && colorNumber[3] * 10 >= total * 7) {
+                color = Color.RED;
+                return;
+            }
+            if (colorNumber[2] >= 1 && colorNumber[2] * 10 <= total) {
+                color = Color.BLUE;
+                return;
+            }
+            return;
+        }
+    }
+
     public double move(cell c) {
         double dis = 0;
-        if (color .equals( Color.red)) {
+        if (color.equals(Color.red)) {
             dis = move_Red(c);
         }
-        if (color .equals( Color.blue)) {
+        if (color.equals(Color.blue)) {
             dis = move_Blue(c);
         }
-        if (color .equals( Color.green)) {
+        if (color.equals(Color.green)) {
             dis = move_Green(c);
         }
-        if (color .equals( Color.YELLOW)) {
+        if (color.equals(Color.YELLOW)) {
             dis = move_Yellow(c);
         }
         return dis;
     }
+
 
     public int getidentity() {
         return Identity;
@@ -195,6 +249,15 @@ public class cell {
 
     public void setPerception(double p) {
         Perception = p;
+    }
+
+    public void setLastColor(Color color) {
+        lastColor = color;
+    }
+
+
+    public Color getLastColor() {
+        return lastColor;
     }
 
 }
